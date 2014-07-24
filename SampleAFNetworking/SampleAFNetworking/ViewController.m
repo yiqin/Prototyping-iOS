188f6d9d146bcc5b9052e651abad8ea8e5111c22
yiqin <yiqin.mems@gmail.com>
2014-07-24 00:11:07 -0400

    Add Batch of Operations.

--- SampleAFNetworking/SampleAFNetworking/ViewController.m
+++ SampleAFNetworking/SampleAFNetworking/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "AFHTTPRequestOperation.h"
 
+
 @interface ViewController ()
 
 @end
@@ -21,22 +22,52 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     // Kimono
-    NSURL *url = [NSURL URLWithString:@"https://www.kimonolabs.com/api/7uqn1y8g?apikey=8541e9ff8ff5291b4d84b9f75550c9b8"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    // 1 - Blue Apron
+    NSURL *url1 = [NSURL URLWithString:@"https://www.kimonolabs.com/api/7uqn1y8g?apikey=8541e9ff8ff5291b4d84b9f75550c9b8"];
+    NSURLRequest *request1 = [NSURLRequest requestWithURL:url1];
+    AFHTTPRequestOperation *operation1 = [[AFHTTPRequestOperation alloc] initWithRequest:request1];
     
-    
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc]
-                                         initWithRequest:request];
-    operation.responseSerializer = [AFJSONResponseSerializer serializer];
-    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"JSON: %@", [responseObject objectForKey:@"results"]);
+    operation1.responseSerializer = [AFJSONResponseSerializer serializer];
+    [operation1 setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Blue Apron - JSON: %@", [responseObject objectForKey:@"results"]);
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         // Handle error
     }];
     
     
-    [operation start];
+    // 2 - Product Hunt
+    NSURL *url2 = [NSURL URLWithString:@"https://www.kimonolabs.com/api/afhn5y42?apikey=8541e9ff8ff5291b4d84b9f75550c9b8"];
+    NSURLRequest *request2 = [NSURLRequest requestWithURL:url2];
+    AFHTTPRequestOperation *operation2 = [[AFHTTPRequestOperation alloc] initWithRequest:request2];
+    
+    operation2.responseSerializer = [AFJSONResponseSerializer serializer];
+    [operation2 setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Product Hunt - JSON: %@", [responseObject objectForKey:@"results"]);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        // Handle error
+    }];
+    
+    
+    NSMutableArray *mutableOperations = [NSMutableArray array];
+    
+    [mutableOperations addObject:operation2];
+    [mutableOperations addObject:operation1];
+
+    // Follow the order in the mutableOperations
+    NSArray *operations = [AFURLConnectionOperation batchOfRequestOperations:mutableOperations progressBlock:^(NSUInteger numberOfFinishedOperations, NSUInteger totalNumberOfOperations) {
+        
+        NSLog(@"%lu of %lu complete", (unsigned long)numberOfFinishedOperations, (unsigned long)totalNumberOfOperations);
+        
+    } completionBlock:^(NSArray *operations) {
+        NSLog(@"All operations in batch complete");
+    }];
+    
+    
+    [[NSOperationQueue mainQueue] addOperations:operations waitUntilFinished:NO];
+    
+    NSLog(@"hello world");
     
     
 }
