7192d70e819ddb19fa2e88292892ae62db6feb88
yiqin <yiqin.mems@gmail.com>
2014-07-25 23:11:44 -0400

    Init HowToLoadHighQualityImages and change to AFNetworkingExample

---
+++ HowToLoadHighQualityImages/HowToLoadHighQualityImages/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HowToLoadHighQualityImages
+//
+//  Created by yiqin on 7/25/14.
+//  Copyright (c) 2014 yipick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HowToLoadHighQualityImages/HowToLoadHighQualityImages/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HowToLoadHighQualityImages
+//
+//  Created by yiqin on 7/25/14.
+//  Copyright (c) 2014 yipick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
