a87d2b336424e070189c599085dcb5123834a6bb
yiqin <yiqin.mems@gmail.com>
2014-07-30 14:59:40 -0400

    init commit

---
+++ WebScraper/WebScraper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebScraper
+//
+//  Created by yiqin on 7/30/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebScraper/WebScraper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WebScraper
+//
+//  Created by yiqin on 7/30/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
