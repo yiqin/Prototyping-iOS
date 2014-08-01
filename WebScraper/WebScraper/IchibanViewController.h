2c3f72129930fbfd7baead6e0eb03d06e74b48ec
yiqin <yiqin.mems@gmail.com>
2014-07-31 22:24:31 -0400

    Add Parse.com

---
+++ WebScraper/WebScraper/IchibanViewController.h
@@ -0,0 +1,13 @@
+//
+//  IchibanViewController.h
+//  WebScraper
+//
+//  Created by yiqin on 7/31/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IchibanViewController : UIViewController
+
+@end
