7323f46fccf2c8ac11686e2a739215890d60922e
yiqin <yiqin.mems@gmail.com>
2014-08-11 15:58:28 -0400

    Save data into FireBase first time.

--- FireBase-Quickstart/FireBase-Quickstart/ViewController.m
+++ FireBase-Quickstart/FireBase-Quickstart/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Firebase/Firebase.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create a reference to a Firebase location
+    Firebase* ref = [[Firebase alloc] initWithUrl:@"https://quickstart.firebaseio.com/"];
+    // Write data to Firebase
+    // [myRootRef setValue:@"Do you have data? You'll love Firebase. 123456"];
+    
+    
+    /*
+    // Read data and react to changes
+    [myRootRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        NSLog(@"%@ -> %@", snapshot.name, snapshot.value);
+    }];
+    */
+    NSDictionary *alanisawesome = @{
+                                    @"full_name" : @"Alan Turing",
+                                    @"date_of_birth": @"June 23, 1912"
+                                    };
+    NSDictionary *gracehop = @{
+                               @"full_name" : @"Grace Hopper",
+                               @"date_of_birth": @"December 9, 1906"
+                               };
+    Firebase* usersRef = [ref childByAppendingPath: @"users"];
+    NSDictionary *users = @{
+                            @"alanisawesome": alanisawesome,
+                            @"gracehop": gracehop
+                            };
+    [usersRef setValue: users];
+    
+    
+    Firebase* alanRef = [usersRef childByAppendingPath: @"alanisawesome"];
+    [alanRef setValue: alanisawesome];
+    
+    Firebase* hopperRef = [usersRef childByAppendingPath: @"gracehop"];
+    [hopperRef setValue: gracehop];
+    
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
